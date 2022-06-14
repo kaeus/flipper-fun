7746f8dc05e60b865c24ea945ef2cdd3f29613b7
Andrew Waters <Andrew.waters@goekos.net>
2022-06-14 13:23:06 -0400

    Added AutoHoff payload

---
+++ AutoHoff/AutoHoff.ps1
@@ -0,0 +1,99 @@
+#############################################################################################################################################
+
+<#
+
+.NOTES 
+	This will take the image you generated and set it as the targets wall paper
+    Copied from https://raw.githubusercontent.com/I-Am-Jakoby/hak5-submissions/main/OMG/Payloads/OMG-Wallpaper-Troll/Wallpaper-Troll.ps1
+#>
+
+Function Set-WallPaper {
+ 
+    <#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+ 
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+  
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+  
+#>
+
+ 
+    param (
+        [parameter(Mandatory = $True)]
+        # Provide path to image
+        [string]$Image,
+        # Provide wallpaper style that you would like applied
+        [parameter(Mandatory = $False)]
+        [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+        [string]$Style
+    )
+ 
+    $WallpaperStyle = Switch ($Style) {
+  
+        "Fill" { "10" }
+        "Fit" { "6" }
+        "Stretch" { "2" }
+        "Tile" { "0" }
+        "Center" { "0" }
+        "Span" { "22" }
+  
+    }
+ 
+    If ($Style -eq "Tile") {
+ 
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+    }
+    Else {
+ 
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+    }
+ 
+    Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+#----------------------------------------------------------------------------------------------------
+
+$filename = "$Env:USERPROFILE\Downloads\dont-hassle-the-hoff.jpg" 
+
+$imgSources = 
+"https://i.imgflip.com/p4fp2.jpg",
+"https://i.pinimg.com/474x/a2/10/89/a21089e4c2537c3b9e6057ec7023462b.jpg"
+
+$imageToGet = $imgSources | Get-Random
+
+Invoke-WebRequest $imageToGet -OutFile $filename
+
+Set-WallPaper -Image "$filename" -Style Fit
